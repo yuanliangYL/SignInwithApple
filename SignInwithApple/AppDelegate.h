3d1baffba4e8c23823780b46f48bf3ad20440d1e
AlbertYuan <yuanliangios@163.com>
2021-10-26 16:49:47 +0800

    Initial commit

---
+++ SignInwithApple/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SignInwithApple
+//
+//  Created by AlbertYuan on 2021/10/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ SignInwithApple/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  SignInwithApple
+//
+//  Created by AlbertYuan on 2021/10/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
